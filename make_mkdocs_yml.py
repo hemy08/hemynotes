bb4ae72037474dd3ecd82581f854caa70057d844
z00579768 <junwei.zhao@huawei.com>
2024-05-13 16:54:43 +0800

    【Title】: 文档刷新 【DTS&Issues&E2E】: 【Description】: 【Root Cause】: 【Solution】: 【Impact Scope】: 【Interface】: 【Document】: 【Self Test Report】:

--- make_mkdocs_yml.py
+++ make_mkdocs_yml.py
@@ -7,6 +7,7 @@ import re
 import json
 import yaml
 
+# script_path = "F:\\code_hemyzhao\\pages\\hemypages"  # 你的文件路径
 script_path = os.getcwd()  # 你的文件路径
 
 script_dir = os.path.basename(script_path)
@@ -19,9 +20,9 @@ nav_str = ""
 
 JSON_STR_SITE_NAME = "site_name"
 JSON_STR_SITE_DESC = "site_description"
-JSON_STR_HOME = "HOME"
 JSON_STR_REPO_URL = "repo_url"
 JSON_STR_COPYRIGHT = "copyright"
+JSON_STR_HOME = "HOME"
 JSON_STR_IGNORE_DIRS = "ignore_dirs"
 JSON_STR_IGNORE_FILE_TYPES = "ignore_file_types"
 JSON_STR_IGNORE_FILES = "ignore_files"
@@ -43,58 +44,54 @@ ignore_file_types = []
 ignore_files = []
 chapters = {}
 chapter_links = {}
+file_name_cvt_list = {}
+external_links = {}
+pages_config = {
+    JSON_STR_SITE_NAME: '',
+    JSON_STR_SITE_DESC: '',
+    JSON_STR_REPO_URL: '',
+    JSON_STR_COPYRIGHT: '',
+    JSON_STR_HOME: '',
+    JSON_STR_IGNORE_DIRS: '',
+    JSON_STR_IGNORE_FILE_TYPES: '',
+    JSON_STR_IGNORE_FILES: '',
+    JSON_STR_DOCS_DIR: '',
+    JSON_STR_CHAPTERS: '',
+    JSON_STR_CHAPTER_RELA_PATH: '',
+    JSON_STR_CHAPTER_LINK: '',
+    JSON_STR_FILE_NAME_CVT: '',
+    JSON_STR_EXT_LINKS: '',
+    JSON_STR_THEME: '',
+    JSON_STR_PLUGINS: '',
+    JSON_STR_EXTRA: '',
+    JSON_STR_EXTRA_CSS: '',
+    JSON_STR_EXTRA_JS: '',
+    JSON_STR_MD_EXTENSIONS: ''
+}
 
 
-def is_ignore_dir(path):
+# 判断文件路径是否在忽略列表中
+def is_ignore(path):
     base = os.path.basename(path)
-    for ignore in ignore_dir_list:
-        if ignore == base:
-            return 1
-    return 0
+    return base in ignore_dir_list or base in ignore_files or any(file.endswith(base) for file in ignore_file_types)
 
 
-def is_ignore_files(file_name):
-    for ignore in ignore_files:
-        if file_name == ignore:
-            return 1
-    return 0
-
-
-def is_ignore_file_types(file):
-    for ignore in ignore_file_types:
-        if file.endswith(ignore):
-            return 1
-    return 0
-
-
-def is_file_or_dir_ignore(name):
-    return is_ignore_dir(name) or is_ignore_files(name) or is_ignore_file_types(name)
-
-
+# 获取文件列表
 def get_file_list(file_path):
-    dir_list = os.listdir(file_path)
-    if not dir_list:
-        return
-    else:
-        return dir_list
+    return os.listdir(file_path)
 
 
+# 文件名转换
 def file_name_cvt(file_name):
-    result = file_name_cvt_list.get(file_name, "none")
-    if result != "none":
-        return result
-    else:
-        return file_name
+    return file_name_cvt_list.get(file_name, file_name)
 
 
+# 删除文件名中的数字前缀
 def del_number_prefix(file_name):
     # 如果目录/文件名带数字前缀，去掉 01_、01-、0.1-、0.1_、01 、01.、
     pattern = re.compile("^[0-9.]*[-_ .]")
     match_obj = re.findall(pattern, file_name)
-    if len(match_obj) != 0:
-        return file_name[len(match_obj[0]):]
-    else:
-        return file_name
+    return file_name[len(match_obj[0]):] if match_obj else file_name
 
 
 def write_files_info(root_dir, obs_path, name, level, fp):
@@ -102,13 +99,8 @@ def write_files_info(root_dir, obs_path, name, level, fp):
     # 去掉.md后缀
     if name.endswith(".md"):
         file_name = name[:len(name) - 3]
-        pos = obs_path.find(root_dir)
-        if docs_dir is None:
-            path = obs_path[pos + len(root_dir + "/") :]
-        else:
-            path = obs_path[pos + len(root_dir + "/") + len(docs_dir + "/"):]
+        path = obs_path[len(root_dir) + 1 + len(docs_dir) + 1:] if docs_dir else obs_path[len(root_dir) + 1:]
         path = path.replace("\\", "/").replace("docs/","")
-
         # 文件名自定义转换
         file_name = file_name_cvt(file_name)
 
@@ -139,7 +131,7 @@ def print_mkdocs_nav(root_dir, file_dir, level, fp):
         return
 
     for name in dir_list:
-        if is_file_or_dir_ignore(name):
+        if is_ignore(name):
             continue
 
         # 是一个目录
@@ -157,69 +149,24 @@ def print_mkdocs_nav(root_dir, file_dir, level, fp):
     return
 
 
-file_name_cvt_list = {}
-pages_config = {}
-external_links = {}
-
-
-def set_pages_config(data, key):
-    if data.get(key) is None:
-        pages_config[key] = None
-    else:
-        pages_config[key] = data[key]
-
-
 def read_pages_conf():
     with open(config_file, 'r', encoding='utf-8') as f:
         res = f.read(-1)
         data = json.loads(res)
-        set_pages_config(data, JSON_STR_SITE_NAME)
-        set_pages_config(data, JSON_STR_SITE_DESC)
-        set_pages_config(data, JSON_STR_COPYRIGHT)
-        set_pages_config(data, JSON_STR_REPO_URL)
-        set_pages_config(data, JSON_STR_HOME)
-        set_pages_config(data, JSON_STR_THEME)
-        set_pages_config(data, JSON_STR_PLUGINS)
-        set_pages_config(data, JSON_STR_EXTRA)
-        set_pages_config(data, JSON_STR_EXTRA_CSS)
-        set_pages_config(data, JSON_STR_EXTRA_JS)
-        set_pages_config(data, JSON_STR_MD_EXTENSIONS)
-        set_pages_config(data, JSON_STR_DOCS_DIR)
-        get_ignore_dir_file_list(data)
-        get_chapters(data)
-        get_file_name_cvt_list(data)
-        get_external_links(data)
+        for key in data:
+            if key in pages_config:
+                pages_config[key] = data[key]
+            else:
+                pages_config[key] = None
+        ignore_dir_list.extend(data[JSON_STR_IGNORE_DIRS].split(";"))
+        ignore_files.extend(data[JSON_STR_IGNORE_FILES].split(";"))
+        ignore_file_types.extend(data[JSON_STR_IGNORE_FILE_TYPES].split(";"))
+        external_links.update(data[JSON_STR_EXT_LINKS])
+        chapters.update(data[JSON_STR_CHAPTERS])
+        file_name_cvt_list.update(data[JSON_STR_FILE_NAME_CVT])
+
         f.close()
     return
-
-
-def get_ignore_dir_file_list(data):
-    dir_list = data[JSON_STR_IGNORE_DIRS]
-    ignore_dir_list.extend(dir_list.split(";"))
-
-    file_list = data[JSON_STR_IGNORE_FILES]
-    ignore_files.extend(file_list.split(";"))
-
-    file_type_list = data[JSON_STR_IGNORE_FILE_TYPES]
-    ignore_file_types.extend(file_type_list.split(";"))
-
-
-def get_external_links(data):
-    links = data[JSON_STR_EXT_LINKS]
-    for key in links:
-        external_links[key] = links.get(key)
-
-
-def get_chapters(data):
-    links = data[JSON_STR_CHAPTERS]
-    for key in links:
-        chapters[key] = links.get(key)
-
-
-def get_file_name_cvt_list(data):
-    cvt_list = data[JSON_STR_FILE_NAME_CVT]
-    for key in cvt_list:
-        file_name_cvt_list[key] = cvt_list.get(key)
 
 
 def write_mkdocs_file(chapter_name, fp):
@@ -231,54 +178,22 @@ def write_mkdocs_file(chapter_name, fp):
     print_mkdocs_nav(script_path, root_dir, 2, fp)
 
 
-def mkdocs_mermaid_cvt(file, old_strs, new_strs):
-    file_data = ""
-    with open(file, "r", encoding="utf-8") as f:
-        for line_str in f:
-            for idx in range(len(old_strs)):
-                if old_strs[idx] in line_str:
-                    line_str = line_str.replace(old_strs[idx], new_strs[idx])
-            file_data += line_str
-    with open(file, "w", encoding="utf-8") as f:
-        f.write(file_data)
-
-
 def write_mkdocs_themes(fp):
-    fp.write("\n\n")
-    yaml.dump({'theme': pages_config[JSON_STR_THEME]}, fp, allow_unicode=True)
-
-    fp.write("\n\n")
-    yaml.dump({'plugins': pages_config[JSON_STR_PLUGINS]}, fp, allow_unicode=True)
-
-    fp.write("\n\n")
-    yaml.dump({'extra': pages_config[JSON_STR_EXTRA]}, fp, allow_unicode=True)
-
-    fp.write("\n\n")
-    yaml.dump({'extra_css': pages_config[JSON_STR_EXTRA_CSS]}, fp, allow_unicode=True)
-
-    fp.write("\n\n")
-    yaml.dump({'extra_javascript': pages_config[JSON_STR_EXTRA_JS]}, fp, allow_unicode=True)
-
-    fp.write("\n\n")
-    yaml.dump({'markdown_extensions': pages_config[JSON_STR_MD_EXTENSIONS]}, fp, allow_unicode=True)
-
-
-def yaml_dump_key_values(f, key, conf):
-    if not pages_config.get(conf) is None:
-        yaml.dump({key: pages_config.get(conf)}, f, allow_unicode=True)
-        f.write("\n")
+    for key in ['theme', 'plugins', 'extra', 'extra_css', 'extra_javascript', 'markdown_extensions']:
+        if not pages_config.get(key) is None:
+            yaml.dump({key: pages_config.get(key)}, fp, allow_unicode=True)
+            fp.write("\n")
 
 
 def create_chapter_mkdocs():
     yml_file = os.path.join(script_path + "/mkdocs.yml")
     # 打开文件句柄
     with open(yml_file, 'w', encoding='utf-8') as f:
-        yaml_dump_key_values(f, 'site_name', JSON_STR_SITE_NAME)
-        yaml_dump_key_values(f, 'site_description', JSON_STR_SITE_DESC)
-        yaml_dump_key_values(f, 'repo_url', JSON_STR_REPO_URL)
-        yaml_dump_key_values(f, 'copyright', JSON_STR_COPYRIGHT)
-        yaml_dump_key_values(f, 'docs_dir', JSON_STR_DOCS_DIR)
-        
+        for key in ['site_name', 'site_description', 'repo_url', 'copyright', 'docs_dir']:
+            if not pages_config.get(key) is None and len(pages_config.get(key)):
+                yaml.dump({key: pages_config.get(key)}, f, allow_unicode=True)
+                f.write("\n")
+
         write_mkdocs_themes(f)
 
         f.write("\nnav:\n")
@@ -297,18 +212,31 @@ def create_chapter_mkdocs():
     return
 
 
+def mkdocs_mermaid_cvt(file, old_strs, new_strs):
+    with open(file, "r", encoding="utf-8") as f:
+        file_data = f.read()
+
+    for old, new in zip(old_strs, new_strs):
+        if old in file_data:
+            file_data = file_data.replace(old, new)
+
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+
+
+old_strs = [
+    "format: '!!python/name:pymdownx.superfences.fence_code_format'",
+    "emoji_generator: '!!python/name:pymdownx.emoji.to_png'",
+    "emoji_generator: '!!python/name:materialx.emoji.to_svg'",
+    "emoji_index: '!!python/name:materialx.emoji.twemoji'"
+]
+new_strs = [
+    "format: !!python/name:pymdownx.superfences.fence_code_format",
+    "emoji_generator: !!python/name:pymdownx.emoji.to_png",
+    "emoji_generator: !!python/name:materialx.emoji.to_svg",
+    "emoji_index: !!python/name:materialx.emoji.twemoji"
+]
+
 read_pages_conf()
 create_chapter_mkdocs()
-mkdocs_mermaid_cvt(os.path.join(script_path + "/mkdocs.yml"),
-                   [
-                       "format: '!!python/name:pymdownx.superfences.fence_code_format'",
-                       "emoji_generator: '!!python/name:pymdownx.emoji.to_png'",
-                       "emoji_generator: '!!python/name:materialx.emoji.to_svg'",
-                       "emoji_index: '!!python/name:materialx.emoji.twemoji'"
-                   ], [
-                       "format: !!python/name:pymdownx.superfences.fence_code_format",
-                       "emoji_generator: !!python/name:pymdownx.emoji.to_png",
-                       "emoji_generator: !!python/name:materialx.emoji.to_svg",
-                       "emoji_index: !!python/name:materialx.emoji.twemoji"
-                   ]
-                   )
+mkdocs_mermaid_cvt(os.path.join(script_path, "mkdocs.yml"), old_strs, new_strs)
